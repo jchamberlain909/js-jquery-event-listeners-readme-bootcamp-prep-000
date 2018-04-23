1409739bab920e41f605218e97322793aab293fd
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-23 19:06:59 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,10 @@ function getIt(){
   $('p').on("click",(e)=>(alert("Hey!")));
 }
 
+function frameIt() {
+  $('img').on('load',(e)=>(this.addClass("tasty")));
+}
+
 $(document).ready(function(){
 
 // call functions here
