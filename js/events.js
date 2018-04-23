1df13ad66b68307cf24ff28f3f578d95cacfc032
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-23 19:17:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,10 +10,25 @@ function frameIt() {
   });
 }
 
+function pressIt() {
+  $("#typing").on("keydown", (e)=>
+    {
+      if(e.which===71){
+        alert("G key pressed");
+      }
+    })
+}
+
+function submitIt() {
+
+}
+
 $(document).ready(function(){
 
 // call functions here
   getIt();
   frameIt();
+  pressIt();
+  submitIt();
 
 });
